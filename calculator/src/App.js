62024c40856633b9e61f5367668acf437c554329
Diana Murtazina <murka8831@yandex.ru>
2022-09-30 20:33:34 +0400

    enabling sass, resetting styles, added header component

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import Header from "./components/Header";
+
 function App() {
-    return <div className="container"></div>;
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
 }
 
 export default App;
---
+++ calculator/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="container">
+                <h1 class="header__title">
+                    Рассчитайте стоимость автомобиля в лизинг
+                </h1>
+            </div>
+        </header>
+    );
+}
