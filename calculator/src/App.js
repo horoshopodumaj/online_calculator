7b8f6f27d249eb8536311733a644acbc8049a332
Diana Murtazina <murka8831@yandex.ru>
2022-09-30 22:34:13 +0400

    adding inputs and styles

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -5,6 +5,57 @@ function App() {
     return (
         <React.Fragment>
             <Header />
+            <form id="form" className="form">
+                <div className="container">
+                    <div className="form__wrapper">
+                        <label class="param form__param">
+                            <p class="param__title">Стоимость автомобиля</p>
+                            <div class="param__details">
+                                <input
+                                    type="text"
+                                    class="param__value"
+                                    value="3 300 000"
+                                    inputmode="decimal"
+                                    id="input-cost"
+                                />
+                                <h2 className="param__mark">₽</h2>
+                            </div>
+                            <div
+                                class="param__range-slider"
+                                id="slider-cost"
+                            ></div>
+                        </label>
+
+                        <label class="param">
+                            <p class="param__title">Первоначальный взнос</p>
+                            <div class="param__details">
+                                <input
+                                    type="text"
+                                    class="param__value"
+                                    value="420 000 ₽"
+                                    id="input-downpayment"
+                                />
+                                <p className="param__mark">13%</p>
+                            </div>
+                            <div class="param__range-slider"></div>
+                        </label>
+
+                        <label class="param">
+                            <p class="param__title">Срок лизинга</p>
+                            <div class="param__details">
+                                <input
+                                    type="text"
+                                    class="param__value"
+                                    value="60"
+                                    id="input-term"
+                                />
+                                <p className="param__mark">мес.</p>
+                            </div>
+                            <div class="param__range-slider"></div>
+                        </label>
+                    </div>
+                </div>
+            </form>
         </React.Fragment>
     );
 }
--- calculator/src/components/Header.js
+++ calculator/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <div className="container">
-                <h1 class="header__title">
+                <h1 className="header__title">
                     Рассчитайте стоимость автомобиля в лизинг
                 </h1>
             </div>
